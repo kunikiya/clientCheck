c5c234206c998f6eb0737c9a13d733af9e0cef16
kunikiya <y-harada@CT834.IRI-CT.LOCAL>
2012-05-10 16:36:45 +0900

    first commit

---
+++ README.txt
@@ -0,0 +1,23 @@
+clientCheck
+
+ユーザーエージェントを基にクライアント端末の種類とキャリアを判別するシンプルなライブラリ。
+
+作成者 kunikiya
+WEBサイト http://kunikiya.jp/
+動作確認バージョン PHP 5.3
+作成日 2012/5/10
+
+isMobile()
+  クライアント端末がガラケーである場合にture、そうでない場合にfalse
+isSp()
+  クライアント端末がスマホである場合にture、そうでない場合にfalse
+isTab()
+  クライアント端末がタブレットである場合にture、そうでない場合にfalse
+isPc()
+  クライアント端末がそれ以外である場合にture、そうでない場合にfalse
+isKddi()
+  クライアント端末のキャリアがauである場合にture、そうでない場合にfalse
+isDocomo()
+  クライアント端末のキャリアがドコモである場合にture、そうでない場合にfalse
+isSoftbank()
+  クライアント端末のキャリアがソフトバンクである場合にture、そうでない場合にfalse
---
+++ clientCheck.class.php
@@ -0,0 +1,163 @@
+<?php
+
+/**
+ * clientCheck
+ *
+ * クライアント端末の判別をユーザーエージェントを基に行う
+ *
+ * @author kunikiya
+ * @website http://kunikiya.jp/
+ * @since PHP 5.3
+ * @create 2012/5/10
+ **/
+
+class clientCheck
+{
+  private $clientCheck;
+
+  public function __construct()
+  {
+    $userAgent = $_SERVER['HTTP_USER_AGENT'];
+    
+    if(
+      //mb_ereg('Mozilla', $userAgent) ||
+      mb_ereg('DoCoMo', $userAgent) ||
+      mb_ereg('KDDI', $userAgent) ||
+      mb_ereg('SoftBank', $userAgent) ||
+      mb_ereg('UP.Browser', $userAgent) ||
+      mb_ereg('Vodafone', $userAgent) ||
+      mb_ereg('J-PHONE', $userAgent)
+    )
+    {
+      $this->clientCheck = 'mobile';
+    }
+    else if(
+      mb_ereg('iPhone', $userAgent) ||
+      mb_ereg('Android', $userAgent) ||
+      mb_ereg('dream', $userAgent) ||
+      mb_ereg('CUPCAKE', $userAgent) ||
+      mb_ereg('blackberry9500', $userAgent) ||
+      mb_ereg('blackberry9530', $userAgent) ||
+      mb_ereg('blackberry9520', $userAgent) ||
+      mb_ereg('blackberry9550', $userAgent) ||
+      mb_ereg('blackberry9800', $userAgent) ||
+      mb_ereg('webOS', $userAgent) ||
+      mb_ereg('incognito', $userAgent) ||
+      mb_ereg('webmate', $userAgent)
+    )
+    {
+      $this->clientCheck = 'sp';
+    }
+    else if(
+      mb_ereg('iPod', $userAgent)
+    )
+    {
+      $this->clientCheck = 'tab';
+    }
+    else
+    {
+      $this->clientCheck = 'pc';
+    }
+    
+    if(mb_ereg('DoCoMo', $userAgent))
+    {
+      $this->clientCarrier = 'docomo';
+    }
+    else if(mb_ereg('KDDI', $userAgent) || mb_ereg('UP.Browser', $userAgent))
+    {
+      $this->clientCarrier = 'kddi';
+    }
+    else if(
+      mb_ereg('SoftBank', $userAgent)
+      mb_ereg('Vodafone', $userAgent) ||
+      mb_ereg('J-PHONE', $userAgent)
+    )
+    {
+      $this->clientCarrier = 'softbank';
+    }
+  }
+  
+  public function isMobile()
+  {
+    if($this->clientCheck === 'mobile')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  public function isSp()
+  {
+    if($this->clientCheck === 'sp')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  public function isTab()
+  {
+    if($this->clientCheck === 'tab')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  public function isPc()
+  {
+    if($this->clientCheck === 'pc')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  public function isKddi()
+  {
+    if($this->clientCarrier === 'kddi')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  public function isDocomo()
+  {
+    if($this->clientCarrier === 'docomo')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  
+  public function isSoftbank()
+  {
+    if($this->clientCarrier === 'softbank')
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
